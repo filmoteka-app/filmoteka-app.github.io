bd9f4cbe773b904c5fc745329491f5246349f719
filmoteka-app <cfi.2016.tk@outlook.com>
2025-10-27 16:29:45 +1000

    Update filmoteka.header-banner.js

--- js/filmoteka.header-banner.js
+++ js/filmoteka.header-banner.js
@@ -1,10 +1,43 @@
-import { API_KEYS, API_ENDPOINTS } from './filmoteka.constants.js';
-
 class FilmotekaHeaderBanner {
     constructor() {
-        this.apiKeys = API_KEYS;
-        this.currentApiKeyIndex = 0;
         this.header = document.querySelector('.header');
+        
+        // Настройки отображения
+        this.settings = {
+            rotation: 20,     // градусы наклона каждой обложки
+            scale: 1.4,       // масштаб каждой обложки
+            gap: 80,          // отступ между постерами в px
+            posterCount: 8    // количество отображаемых постеров
+        };
+
+        // Массив постеров
+        this.posters = [
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/430042eb-ee69-4818-aed0-a312400a26bf/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/10900341/caf9f155-1a19-42f1-a0f3-9c8773e9083e/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/0b76b2a2-d1c7-4f04-a284-80ff7bb709a4/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/637271d5-61b4-4e46-ac83-6d07494c7645/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/4057c4b8-8208-4a04-b169-26b0661453e3/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/617303b7-cfa7-4273-bd1d-63974bf68927/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/e410c71f-baa1-4fe5-bb29-aedb4662f49b/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/3560b757-9b95-45ec-af8c-623972370f9d/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/4483445/1e2ed281-b1e8-4083-b721-3ece7afc1031/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/a2d5bcae-a1a9-442f-8195-f5373a5ba77f/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/ae3b699c-3db7-4196-a869-39b610bfe706/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/4716873/85b585ea-410f-4d1c-aaa5-8d242756c2a4/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/7c3460dc-344d-433f-8220-f18d86c8397d/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/4716873/0a07a903-9025-4aff-bf7c-46bbb175888c/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/772093e4-7f68-49aa-a805-d654693aee26/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/8662d92a-5881-4600-a7ae-549e6fd53b03/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/0fa5bf50-d5ad-446f-a599-b26d070c8b99/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/e9008e2f-433f-43b0-b9b8-2ea8e3fb6c9b/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/80eab631-346c-4c29-b14d-1fa1438158f9/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/9e9e2b2c-a3c1-462e-8d84-e6a19fbe5b9c/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/7ade06a8-4178-4386-9ee2-87fec5a172eb/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/73cf2ed0-fd52-47a2-9e26-74104360786a/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/4774061/cf1970bc-3f08-4e0e-a095-2fb57c3aa7c6/360',
+            'https://avatars.mds.yandex.net/get-kinopoisk-image/1898899/27ed5c19-a045-49dd-8624-5f629c5d96e0/360'
+        ];
+
         this.init();
     }
 
@@ -18,7 +51,7 @@ class FilmotekaHeaderBanner {
     }
 
     async createBanner() {
-        const posters = await this.getRandomPosters(4);
+        const posters = this.getRandomPosters(this.settings.posterCount);
         if (posters.length > 0) {
             await this.renderCollage(posters);
         } else {
@@ -26,59 +59,10 @@ class FilmotekaHeaderBanner {
         }
     }
 
-    async getRandomPosters(count) {
-        try {
-            const response = await this.safeFetch(`${API_ENDPOINTS.TOP}?type=TOP_250_BEST_FILMS&page=1`);
-            const data = await response.json();
-            
-            if (!data.films || data.films.length === 0) {
-                return [];
-            }
-
-            const shuffled = [...data.films].sort(() => 0.5 - Math.random());
-            const selectedFilms = shuffled.slice(0, count);
-            
-            return selectedFilms
-                .filter(film => film.posterUrlPreview || film.posterUrl)
-                .map(film => film.posterUrlPreview || film.posterUrl);
-                
-        } catch (error) {
-            console.error('Error fetching posters for header:', error);
-            return [];
-        }
-    }
-
-    async safeFetch(url) {
-        for (let attempt = 0; attempt < this.apiKeys.length; attempt++) {
-            const key = this.apiKeys[this.currentApiKeyIndex];
-            try {
-                const controller = new AbortController();
-                const timeoutId = setTimeout(() => controller.abort(), 5000);
-
-                const response = await fetch(url, {
-                    headers: {
-                        'X-API-KEY': key,
-                        'Content-Type': 'application/json'
-                    },
-                    signal: controller.signal
-                });
-
-                clearTimeout(timeoutId);
-
-                if (response.ok) {
-                    return response;
-                }
-
-                this.switchApiKey();
-            } catch (err) {
-                this.switchApiKey();
-            }
-        }
-        throw new Error('All API keys failed');
-    }
-
-    switchApiKey() {
-        this.currentApiKeyIndex = (this.currentApiKeyIndex + 1) % this.apiKeys.length;
+    getRandomPosters(count) {
+        // Перемешиваем массив и берем нужное количество
+        const shuffled = [...this.posters].sort(() => 0.5 - Math.random());
+        return shuffled.slice(0, count);
     }
 
     async renderCollage(posters) {
@@ -90,7 +74,7 @@ class FilmotekaHeaderBanner {
         posterRow.style.flexDirection = 'row';
         posterRow.style.alignItems = 'center';
         posterRow.style.justifyContent = 'center';
-        posterRow.style.gap = '80px';
+        posterRow.style.gap = `${this.settings.gap}px`;
         posterRow.style.width = '100%';
         posterRow.style.height = '100%';
         posterRow.style.position = 'absolute';
@@ -106,7 +90,6 @@ class FilmotekaHeaderBanner {
             posterItem.style.flexShrink = '0';
             posterItem.style.height = '100%';
             posterItem.style.position = 'relative';
-            posterItem.style.margin = '0 0px';
             
             const img = document.createElement('img');
             img.src = posterUrl;
@@ -117,7 +100,8 @@ class FilmotekaHeaderBanner {
             img.style.display = 'block';
             img.style.objectFit = 'contain';
             
-            posterItem.style.transform = 'rotate(20deg) scale(1.4)';
+            // Применяем настройки трансформации
+            posterItem.style.transform = `rotate(${this.settings.rotation}deg) scale(${this.settings.scale})`;
             posterItem.style.transformOrigin = 'center';
             
             posterItem.appendChild(img);
